3d1ce5dae8c8bb17264e108428f6af185c9d0fdc
Valentin Charbonnier <valentinbreiz@gmail.com>
2023-07-20 14:18:41 +0200

    ✨ Implement user service

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const logs_services = require('./proto/js/logs_grpc_pb');
 const hello_services = require('./proto/js/helloworld_grpc_pb');
+const user_services = require('./proto/js/user_grpc_pb');
 const { streamLogs } = require('./services/logs');
 const { sayHello } = require('./services/hello');
-const GrpcServer = require('./GrpcServer');
+const { SignIn, SignUp, SignOut, ResetPassword, ChangeEmail } = require('./services/user');
+const GrpcServer = require('./grpcServer');
 
 /**
  * Starts an RPC server that receives requests for the Logs service at the
@@ -10,8 +12,22 @@ const GrpcServer = require('./GrpcServer');
  */
 function main() {
   const server = new GrpcServer();
+
+  //GRPC user service
+  server.addService(user_services.UserService, {
+    SignIn: SignIn,
+    SignUp: SignUp,
+    SignOut: SignOut,
+    ResetPassword: ResetPassword,
+    ChangeEmail: ChangeEmail
+  });
+
+  //GRPC stream log service
   server.addService(logs_services.LogsService, {streamLogs : streamLogs});
+
+  //GRPC hello world service
   server.addService(hello_services.GreeterService, {sayHello : sayHello});
+
   server.start(50051);
 }
 
---
+++ src/services/user.js
@@ -0,0 +1,94 @@
+const { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail, updateEmail } = require("firebase/auth");
+const firebaseConfig = require("../firebaseConfig.json");
+const { initializeApp } = require('firebase/app');
+const messages = require('../proto/js/user_pb');
+
+const firebase = initializeApp(firebaseConfig);
+const auth = getAuth(firebase);
+let user = null;
+
+function SignIn(call, callback) {
+    const email = call.request.getEmail();
+    const password = call.request.getPassword();
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            user = userCredential.user;
+            const token = user.stsTokenManager.accessToken;
+
+            const reply = new messages.UserResponse();
+            reply.setMessage('User signed in successfully');
+            reply.setToken(token);
+            callback(null, reply);
+        })
+        .catch((error) => {
+            callback(error);
+        });
+}
+
+function SignUp(call, callback) {
+    const email = call.request.getEmail();
+    const password = call.request.getPassword();
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            user = userCredential.user;
+
+            const reply = new messages.UserResponse();
+            reply.setMessage('User created successfully');
+            reply.setToken(user.stsTokenManager.accessToken);
+            callback(null, reply);
+        })
+        .catch((error) => {
+            callback(error);
+        });
+}
+
+function SignOut(call, callback) {
+    user = null;
+    auth.signOut()
+        .then(() => {
+            const reply = new messages.EmptyResponse();
+            reply.setMessage('User signed out successfully');
+            callback(null, reply);
+        })
+        .catch((error) => {
+            callback(error);
+        });
+}
+
+function ResetPassword(call, callback) {
+    const email = call.request.getEmail();
+
+    sendPasswordResetEmail(auth, email)
+        .then(() => {
+            const reply = new messages.UserResponse();
+            reply.setMessage('Reset email sent successfully');
+            callback(null, reply);
+        })
+        .catch((error) => {
+            callback(error);
+        });
+}
+
+function ChangeEmail(call, callback) {
+    const newEmail = call.request.getEmail();
+
+    updateEmail(auth.currentUser, newEmail)
+        .then(() => {
+            const reply = new messages.UserResponse();
+            reply.setMessage('Change successfully');
+            callback(null, reply);
+        })
+        .catch((error) => {
+            callback(error);
+        });
+}
+
+module.exports = {
+    SignIn: SignIn,
+    SignUp: SignUp,
+    SignOut: SignOut,
+    ResetPassword: ResetPassword,
+    ChangeEmail: ChangeEmail
+};
