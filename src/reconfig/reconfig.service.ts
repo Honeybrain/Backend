1f3ba472cd5f9ea2b5e729db96b6e3d7a95e63cb
Francois <francois.bolla@epitech.eu>
2024-03-20 11:29:49 +0100

    reconfig done

--- src/reconfig/reconfig.service.ts
+++ src/reconfig/reconfig.service.ts
@@ -4,29 +4,64 @@ import { ReConfigDto } from './_utils/dto/request/reconfig-request.dto';
 import * as Docker from 'dockerode';
 import { RpcException } from '@nestjs/microservices';
 
+interface dummies {
+  num_services: number;
+  ip_addresses: Array<string>;
+} 
+
+interface ftps {
+  ip_address: string;
+  port: string;
+}
+
+interface NewConfig {
+  dummy_pc: dummies;
+  ftp: ftps;
+}
+
+function delay(ms: number) {
+  return new Promise( resolve => setTimeout(resolve, ms) );
+}
 
 @Injectable()
 export class ReconfigureService {
 
   private docker = new Docker();
 
+/*private validate_config(configJson: NewConfig) {
 
-public async ReconfigHoneypot(Reconfig: ReConfigDto) {
-    //check config
-    if (!Reconfig.config) throw new RpcException('Config cannot be empty')
-    //recup liste dockers
-    const network = await this.docker.getNetwork('honeypot_network').inspect();
+  if (configJson.dummy_pc.num_services = 0)
+    return
+}*/
 
-    const networkContainers = network.Containers;
-    const containerIds = Object.keys(networkContainers);
+public async reconfigHoneypot(Reconfig: ReConfigDto) {
+  //check config
+  if (!Reconfig.config) throw new RpcException('Config cannot be empty')
 
-    const containers = await this.docker.listContainers({ all: true });
+  //recup liste dockers
+  const containers = await this.docker.listContainers({ all: true });
+  const buildContainers = containers.filter((container) => container.Names[0].startsWith('/honeypot_'));
 
-    const buildContainers = containers.filter((container) => container.Names[0].startsWith('/honeypot_'));
-
-    buildContainers.forEach(element => {
-      this.docker.getContainer('fb607059a048').stop();
-    });
+  buildContainers.forEach(element => {
+    this.docker.getContainer(element.Id).stop().catch((e) => console.log(e));
+  });
+  
   //creer docker depuis config
+  let configJson: NewConfig = JSON.parse(Reconfig.config);
+
+  //this.validate_config(configJson);
+  await delay(1000)
+  var newDocker = new Docker()
+  //newDocker.createContainer({Image: 'ubuntu', Cmd: ['/bin/bash'], name: 'honeypot_dummy_pc_123'}, function(err, container){container?.start({})})
+  for (let i=0; i < configJson.dummy_pc.num_services; i++) {
+    newDocker.run('ubuntu', ['bash', '-c', 'sleep infinity'], process.stdout, {Hostname: configJson.dummy_pc.ip_addresses[i], name:'honeypot_dummy_pc_'+i},function() {
+      console.log("running new dummy pc on " + configJson.dummy_pc.ip_addresses[i]);
+    });
+  }
+  if (configJson.ftp.ip_address != "") {
+    newDocker.run('ubuntu', ['bash', '-c', 'sleep infinity'], process.stdout, {Hostname: configJson.ftp.ip_address, name:'honeypot_dummy_ftp_1 '},function() {
+      console.log("running new dummy ftp on " + configJson.ftp.ip_address + " on port " + configJson.ftp.port);
+    })
+  }
   }
 }
